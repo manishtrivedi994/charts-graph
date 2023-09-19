4b13ecd2704edf3a4d15a4d9d2581fb8550cfe20
Manish Trivedi <manish.trivedi1@cars24.com>
2023-09-19 17:38:11 +0530

    chart added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import MultipleAxesChart from './components/MultipleAxesChart';
 import ColumnChart from './components/ColumnChart';
+import StackedColumnChart from './components/StackedColumnChart';
 
 function App() {
   return (
     <div className="App">
      <MultipleAxesChart />
      <ColumnChart />
+     <StackedColumnChart />
     </div>
   );
 }
