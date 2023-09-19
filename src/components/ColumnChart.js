a85d84c3d58a53ab12b9d6ab195fdc87068edc6a
Manish Trivedi <manish.trivedi1@cars24.com>
2023-09-19 19:26:23 +0530

    fixing styles and components

--- src/components/ColumnChart.js
+++ src/components/ColumnChart.js
@@ -5,35 +5,11 @@ import CanvasJSReact from '@canvasjs/react-charts';
 
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-export default function ColumnChart () {
-    const options = {
-        title: {
-            text: "Total Accounts Receivable and Payable Aging"
-        },
-        data: [
-        {
-            type: "column",
-            dataPoints: [
-                { label: "Current",  y: 10  },
-                { label: "1-30", y: 15  },
-                { label: "31-40", y: 25  },
-                { label: "41-50",  y: 30  },
-                { label: "51+",  y: 28  }
-            ]
-        },
-        {
-            type: "column",
-            dataPoints: [
-                { label: "Current",  y: 7  },
-                { label: "1-30", y: 26  },
-                { label: "31-40", y: 13  },
-                { label: "41-50",  y: 45  },
-                { label: "51+",  y: 5  }
-            ]
-        }
-        ]
-    }
+export default function ColumnChart ({title='', data={}}) {
     return (
-        <CanvasJSChart options = {options}/>
+        <div>
+            <div>{title}</div>
+            <CanvasJSChart options = {data}/>
+        </div>
     )
 }
